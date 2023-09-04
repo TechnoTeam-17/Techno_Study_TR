524fa0d0e03b3352ec796f04572731fb6ec1b9d1
alpaslanalemdarGEOPETRA <alpaslanalemdar5406@hotmail.com>
2023-09-04 23:32:52 +0200

    BaseDriver Baslangıç İşlemleri >> Sayfa Acma . Yapıldı

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -30,23 +30,12 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        loginTesti();
+        sayfaAcma();
     }
 
-    public void loginTesti(){
-        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
-        MyFunc.Bekle(2);
+    public void sayfaAcma(){
+        driver.get("https://techno.study/tr");
 
-        WebElement inputEmail = driver.findElement(By.id("input-email"));
-        inputEmail.sendKeys("testng1@gmail.com");
-
-        WebElement inputpassword = driver.findElement(By.id("input-password"));
-        inputpassword.sendKeys("123qweasd");
-
-        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
-        loginBtn.click();
-
-        Assert.assertTrue(driver.getTitle().equals("My Account"));
     }
 
     @AfterClass
---
+++ src/anasayfaKurslarDropDownGoruntuleme.java
@@ -0,0 +1,2 @@
+public class anasayfaKurslarDropDownGoruntuleme extends {
+}
