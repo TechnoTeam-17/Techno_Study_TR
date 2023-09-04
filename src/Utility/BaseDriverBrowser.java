d02124046e4182bd47b7e0fd8407dac2d1d9268c
alpaslanalemdarGEOPETRA <alpaslanalemdar5406@hotmail.com>
2023-09-05 00:06:56 +0200

    BaseDriverBrowser BeforeClass düzenlendi. Parallel çalışma ve Site url lerini parametre olarak göndermek mümkün. Böylece @Test yazarak site ismini her bowser için tekrar çağırmaya gerek olmayacak. Lütfen sadece kendi Class'ınızı Git>>Add olarak ayarlayın. Kendi site isminizi girince Conflict olmasın.

--- src/Utility/BaseDriverBrowser.java
+++ src/Utility/BaseDriverBrowser.java
@@ -24,8 +24,8 @@ public class BaseDriverBrowser {
     public static WebDriverWait wait;
 
     @BeforeClass
-    @Parameters("browserTipi")
-    public void baslangicIslemleri(String browserTipi){
+    @Parameters({"browserTipi", "siteIsmi"})
+    public void baslangicIslemleri(String browserTipi, String calisacakSiteIsmi){
         Logger logger= Logger.getLogger(""); // output yapılan logları al.
         logger.setLevel(Level.SEVERE); // sadece ERROR ları göster
 
@@ -52,24 +52,10 @@ public class BaseDriverBrowser {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        loginTesti();
+
+        driver.get(calisacakSiteIsmi);
     }
 
-    public void loginTesti(){
-        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
-        MyFunc.Bekle(2);
-
-        WebElement inputEmail = driver.findElement(By.id("input-email"));
-        inputEmail.sendKeys("testng1@gmail.com");
-
-        WebElement inputpassword = driver.findElement(By.id("input-password"));
-        inputpassword.sendKeys("123qweasd");
-
-        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
-        loginBtn.click();
-
-        Assert.assertTrue(driver.getTitle().equals("My Account"));
-    }
 
     @AfterClass
     public void bitisIslemleri(){ // tearDown
--- src/anasayfaKurslarDropDownGoruntuleme.java
+++ src/anasayfaKurslarDropDownGoruntuleme.java
@@ -1,2 +1,13 @@
-public class anasayfaKurslarDropDownGoruntuleme extends {
+import Utility.BaseDriver;
+import Utility.BaseDriverBrowser;
+import org.testng.annotations.Test;
+
+public class anasayfaKurslarDropDownGoruntuleme extends BaseDriverBrowser {
+    @Test
+    public void anasayfaKurslarDropDownGoruntulemeTest(){
+
+
+
+
+    }
 }
